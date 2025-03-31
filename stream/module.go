bcbbca44cb22f8203046aa17a02f07bd2d8bca3f
Sadegh <me@sadegh.io>
2025-03-31 15:49:24 +0330

    feat: added jetstream module

---
+++ stream/module.go
@@ -0,0 +1,29 @@
+package cache
+
+import (
+	"github.com/nats-io/nats.go"
+	"github.com/nats-io/nats.go/jetstream"
+	"go.uber.org/fx"
+)
+
+var MODULE = fx.Module(
+	"STREAM",
+	fx.Provide(New),
+)
+
+type Config struct {
+	Credentials string `yaml:"credentials"`
+	Address     string `yaml:"address"`
+	Client      string `yaml:"client"`
+}
+
+func New(config Config) jetstream.JetStream {
+	nc, _ := nats.Connect(
+		config.Address,
+		nats.Name(config.Client),
+		nats.UserCredentials(config.Credentials))
+
+	js, _ := jetstream.New(nc)
+
+	return js
+}
